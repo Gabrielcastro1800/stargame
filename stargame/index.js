a8ce876b5bb59ebe9f38a578b06cd4f5d0eb9d95
Gabrielcastro1800 <118899849+Gabrielcastro1800@users.noreply.github.com>
2024-12-30 13:19:58 -0300

    sprites

--- stargame/index.js
+++ stargame/index.js
@@ -1,11 +1,16 @@
 let canvas = document.getElementById("canvas");
 let c = canvas.canvas.getContext("2d");
 let tela = 1
+
+let images = []
+images[0]= new Image()
+images[0].src = "sprites/kidgoku.png"
+
 setInterval(main(),17);
 
 function main(){
 if( tela == 1){
- 
+c.drawImage()
 
 }
 }
