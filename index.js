687761365712d6aa1bfc3dd150a73f6c9342d40a
Gabrielcastro1800 <118899849+Gabrielcastro1800@users.noreply.github.com>
2024-12-30 15:01:03 -0300

    size

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ let canvas = document.getElementById("canvas");
 let c = canvas.getContext("2d");
 let tela = 1
 c.imageSmoothingEnabled = false;
+
+
 let images = []
 images[0] = new Image()
 images[0].src = "sprites/kidgoku.png"
+ // chars = [[name,image id,base hp,base str,base def,base speed,(mana,ki,chakra,etc),stamina],[]]
+let chars = [["Kid goku",0,150,10,7,5,100,20]]
+let slots = []
+
 setInterval(main(),500);
 
 function main(){
 if( tela == 1){
-c.drawImage(images[0],100,100)
+c.drawImage(images[0],100,100,128,128)
 }
 }
