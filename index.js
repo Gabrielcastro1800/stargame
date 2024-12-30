185e69ed0ddc5a9841391bd23fc6ae96b4757e7a
Gabrielcastro1800 <118899849+Gabrielcastro1800@users.noreply.github.com>
2024-12-30 13:40:51 -0300

    7272

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let c = canvas.canvas.getContext("2d");
 let tela = 1
 
 let images = []
-images[0]= new Image()
+images[0] = new Image()
 images[0].src = "sprites/kidgoku.png"
 
 setInterval(main(),17);
