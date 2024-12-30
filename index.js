9f42522165161b1b3976bce03499d5b66fa05ebc
Gabrielcastro1800 <118899849+Gabrielcastro1800@users.noreply.github.com>
2024-12-30 13:52:10 -0300

    alert

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ let tela = 1
 let images = []
 images[0] = new Image()
 images[0].src = "sprites/kidgoku.png"
-
+alert("4")
 setInterval(main(),500);
-
+alert("3")
 function main(){
 if( tela == 1){
+alert("1")
 c.drawImage(images[0],100,100)
-
+alert("2")
 }
 }
