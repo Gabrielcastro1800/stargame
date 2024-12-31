8e639651abc4cef3cf312371e26c356d98779beb
Gabrielcastro1800 <118899849+Gabrielcastro1800@users.noreply.github.com>
2024-12-31 17:46:41 -0300

    42

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ images[1] = new Image()
 images[1].src = "sprites/kingpiccolo.png"
 images[2] = new Image()
 images[2].src = "sprites/superman.png"
- // chars = [[name,image id,base hp,base str,base def,base speed,(mana,ki,chakra,etc),stamina,skills,statsMultiplier,passisves],[]]
-let chars = [["Kid goku",0,150,10,7,5,100,20,"Martial arts Attack",1.07,],
-["King Piccolo",1,120,5,8,10,200,10,"Martial arts Attack","Limbs Regen","Explosive Demon Wave",1.04,],
-["Superman",2,1500,50,200,120,0,450,"Holding punch","Frost breath","Heat Vision","Xray vision","Super Punch",1.005,"Kripitonite weakness","Sound weakness"]
+ // chars = [[name,image id,base hp,base str,base def,base speed,(mana,ki,chakra,etc),stamina,skills,statsMultiplier,passisves,lvl],[]]
+let chars = [["Kid goku",0,150,10,7,5,100,20,"Martial arts Attack",1.07,,1],
+["King Piccolo",1,120,5,8,10,200,10,"Martial arts Attack","Limbs Regen","Explosive Demon Wave",1.04,,1],
+["Superman",2,1500,50,200,120,0,450,"Holding punch","Frost breath","Heat Vision","Xray vision","Super Punch",1.005,"Kripitonite weakness","Sound weakness",1]
 ]
 let slots = []
 slots = chars
@@ -28,10 +28,10 @@ if( tela == 1){
         c.drawImage(slots[0][1],100,200,128,128)
     }
     if(!(slots[1] == null)){
-        c.drawImage(slots[1][1],200,500,128,128)
+        c.drawImage(slots[1][1],400,500,128,128)
     }
     if(!(slots[2] == null)){
-        c.drawImage(slots[2][1],100,600,128,128)
+        c.drawImage(slots[2][1],100,700,128,128)
     }
 }
 }
